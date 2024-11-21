a3dd189df5f9533c5ef416de0782fae7e4c46598
diyaniliev96 <diyan.iliev96@gmail.com>
2024-11-21 19:33:46 +0200

    Task 01 find middle note

---
+++ src/find_middle_note/LinkedList.java
@@ -0,0 +1,88 @@
+package find_middle_note;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // WRITE FIND MIDDLE NODE METHOD HERE //
+    //                                    //
+    //                                    //
+    //                                    //
+    //                                    //
+    ////////////////////////////////////////
+    public Node findMiddleNode() {
+        Node slowPointer = head;
+        Node fasterPointer = slowPointer;
+        if (slowPointer == null) {
+            return null;
+        }
+        while(fasterPointer != null && fasterPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fasterPointer = fasterPointer.next.next;
+        }
+        return slowPointer;
+    }
+}
