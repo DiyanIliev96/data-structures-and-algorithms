7d965372c47250615d81eba2ff739aec72c33463
diyaniliev96 <diyan.iliev96@gmail.com>
2025-01-16 12:47:35 +0200

    Add task: prepend method

--- src/linked_list/lecture_tasks/prepend/Main.java
+++ src/linked_list/lecture_tasks/prepend/Main.java
@@ -1,6 +1,18 @@
 package linked_list.lecture_tasks.prepend;
 
-public class Main {
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Main {
     public static void main(String[] args) {
 
         LinkedList myLinkedList = new LinkedList(2);
