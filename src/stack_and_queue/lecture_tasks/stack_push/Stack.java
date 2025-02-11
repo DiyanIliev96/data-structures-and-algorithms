085d2760426deef80df558c83129d9f744611da4
diyaniliev96 <diyan.iliev96@gmail.com>
2025-02-11 17:18:01 +0200

    Add task: stack push method

---
+++ src/stack_and_queue/lecture_tasks/stack_push/Stack.java
@@ -0,0 +1,70 @@
+package stack_and_queue.lecture_tasks.stack_push;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public Node getTop() {
+        return top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (height == 0) {
+            System.out.println("Top: null");
+        } else {
+            System.out.println("Top: " + top.value);
+        }
+        System.out.println("Height:" + height);
+        System.out.println("\nStack:");
+        if (height == 0) {
+            System.out.println("empty");
+        } else {
+            printStack();
+        }
+    }
+
+    public void makeEmpty() {
+        top = null;
+        height = 0;
+    }
+
+    /// WRITE PUSH METHOD HERE ///
+    //                          //
+    //                          //
+    //                          //
+    //                          //
+    //////////////////////////////
+    public void push(int value) {
+        Node newNode = new Node(value);
+        newNode.next = top;
+        top = newNode;
+        height++;
+    }
+}
